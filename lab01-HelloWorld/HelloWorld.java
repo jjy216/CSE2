87ff1ac69b3b9442f071f8a5f41a670f0cdea3cf
jjy216 <jjy216@lehigh.edu>
2015-01-23 15:48:11 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,17 @@
+//////////////////////////////////////////////////////////////////////////
+//Jason Yang
+//Hello World Java Program
+//01.23.2015
+
+public class HelloWorld{
+    public static void main(String[] args) {
+        System.out.println("################################");
+        System.out.println("################################");
+        System.out.println("#                              #");
+        System.out.println("#      Hello World 2015!!      #");
+        System.out.println("#                              #");
+        System.out.println("################################");
+        System.out.println("################################");
+        System.out.println("test");
+    }
+}
