e78ee2a6afab19dc7317e1da0ca375e66caa961a
jjy216 <jjy216@lehigh.edu>
2015-01-24 00:44:47 +0000

    WelcomeClass.java

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,19 @@
+//////////////////////////////////////////////////////////////////////////
+//Jason Yang
+//HW-01 Java Program
+//01.26.2015
+
+public class WelcomeClass{
+    public static void main(String[] args) {
+        System.out.println("    -----------");
+        System.out.println("    | WELCOME |");
+        System.out.println("    -----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+        System.out.println("<-J--J--Y--2--1--6->");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v");
+        System.out.println("During the summer of 2014, I had an opportunity to work as a paid-intern at LG Display. I was in the mobile developement team. I learned that LG Display is the best out of all. However I also learned that being an intern was not an easy thing to do. In the end, I earned a thousand dollars for working for only a month.");
+     
+    }
+}
