742531935ae62969f7baa38acf5111419d38b371
jjy216 <jjy216@lehigh.edu>
2015-02-09 03:54:23 +0000

    updated hw03 FourDigits

---
+++ hw03/FourDigits.java
@@ -0,0 +1,30 @@
+//////////////////////////////////////////////////////////////////////////
+//Jason Yang
+//HW-03 FourDigits
+//02.10.2015
+//This program will allow me to show the first four decimals
+
+import java.util.Scanner;//Import the scanner
+//Define class
+public class FourDigits {
+    //main method
+    public static void main(String[] args) {
+        Scanner DigitScanner;
+        DigitScanner = new Scanner( System.in );
+        //Scanner setup complete
+        System.out.print("Enter a double with four decimal points and I display the four digits to the right of a decimal point- ");
+        Double RandomDouble = DigitScanner.nextDouble();
+
+        System.out.println("The four digits are "+output3+".");
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    } //end of main method
+} //end of program
